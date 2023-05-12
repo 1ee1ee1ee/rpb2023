bcae7780fbb2e325f1c258e6102a3935cd4cc63f
JIHUN JUNG <129812314+rpb2023@users.noreply.github.com>
2023-05-12 09:49:17 +0900

    Add files via upload

---
+++ my_kmeans.m
@@ -0,0 +1,23 @@
+clear all; close all;
+
+x=load("data.txt")
+
+figure(1);
+plot(x(:,1),x(:,2),'k.');
+title('Randomly Generated Data');
+
+opts=statset('Display','final');
+[idx,C]=kmeans(x,2,'Distance','cityblock', ...
+    'Options',opts);
+figure(2);
+plot(x(idx==1,1),x(idx==1,2),'r.','MarkerSize',12)
+hold on
+plot(x(idx=2,1),x(idx=2,2),'b.','MarkerSize',12)
+plot(C(:,1),C(:,2),'kx', ...
+    'MarkerSize',15,'LineWidth',3)
+legend('Cluster 1','Cluster 2','Centroids', ...
+    'Location','NW')
+title 'Cluster Assignments and Centroids'
+hold off
+
+
